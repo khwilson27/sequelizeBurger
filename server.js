f8031fe98219d6035f24e8b88f5751890e48e2d0
Kevin Wilson <kevinhn27@gmail.com>
2017-06-29 20:26:32 -0700

    controller converted, server not starting

--- server.js
+++ server.js
@@ -2,13 +2,12 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
 
-var port = process.env.PORT || 3000;
+var PORT = process.env.PORT || 3000;
 
 var app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
-
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // Override with POST having ?_method=DELETE
@@ -21,8 +20,11 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-var routes = require("./controllers/burgers_controller.js");
-
+var routes = require("./controllers/burgers_controller.js")(app);
 app.use("/", routes);
 
-app.listen(port);
+db.sequelize.sync().then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+});
